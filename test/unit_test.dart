8d4f533354e7585452f5979d800a4f304e1d5234
paprikaparty33 <88854349+paprikaparty33@users.noreply.github.com>
2023-02-11 20:19:30 +0300

    Update unit_test.dart

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:trpokp/table1.dart';
-import 'package:trpokp/table1.dart';
-import 'package:trpokp/database.dart';
-import 'package:trpokp/worker.dart';
 
-void main() {
-  test('Testing decreament func', () {});
-
-  int x = 15;
-  String category = 'a';
-}
